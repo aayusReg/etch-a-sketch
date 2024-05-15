67ce80f540d53e90017c9ef51e401e1e523d8272
Aayush Regmi <aayu.regmi10@gmail.com>
2024-05-15 07:42:32 +0545

    Edit the code to make grid more easy and make hover effect

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 const container = document.querySelector(".container");
-let divs = [];
-for (let i = 0; i < 16; i++) {
-  for (let j = 0; j < 16; j++) {
-    let div = document.createElement("div");
-    divs.push(div);
+const btn = document.querySelector("button");
+
+function onClick() {
+  container.innerHTML = "";
+
+  let num = +prompt("Enter the grid size: ","16");
+  if (num > 100) {
+    alert("Choose a smaller number");
+  } else if (isNaN(num) || num === null || num === "" || num === " ") {
+    alert("Please choose a valid number");
+  } else {
+    for (let i = 0; i < num; i++) {
+      let column = document.createElement("div");
+      column.setAttribute("class", "div");
+      for (let j = 0; j < num; j++) {
+        let row = document.createElement("div");
+        row.setAttribute("class", "div");
+        row.style.border = "2px solid black";
+        column.appendChild(row);
+        row.addEventListener('mouseenter',()=>{
+            row.classList.toggle('hover')
+        })
+      }
+      container.appendChild(column)
+    }
   }
 }
-
-divs.forEach((div) => {
-  div.setAttribute("class", "div");
-  div.addEventListener("mouseenter", () => {
-    div.classList.toggle("hover");
-  });
-
-  container.appendChild(div);
-});
+btn.addEventListener("click", onClick);
