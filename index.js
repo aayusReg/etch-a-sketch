2c4314e0eb428ec766b0e118d1c393c4e84de4ff
Aayush Regmi <aayu.regmi10@gmail.com>
2024-05-15 05:49:24 +0545

    Add hover effect

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const container = document.querySelector(".container");
-let div;
+let divs = [];
 for (let i = 0; i < 16; i++) {
   for (let j = 0; j < 16; j++) {
-    div = document.createElement("div");
-    div.setAttribute('class','div')
-    container.appendChild(div);
+    let div = document.createElement("div");
+    divs.push(div);
   }
 }
 
+divs.forEach((div) => {
+  div.setAttribute("class", "div");
+  div.addEventListener("mouseenter", () => {
+    div.classList.toggle("hover");
+  });
+
+  container.appendChild(div);
+});
