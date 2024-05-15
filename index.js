dde12933fb11fb2c2ac58faab9e7199b68eb7c28
Aayush Regmi <aayu.regmi10@gmail.com>
2024-05-15 08:08:33 +0545

    Add functionality to display grid on start and on reload

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 const container = document.querySelector(".container");
 const btn = document.querySelector("button");
+const btn2 = document.querySelector(".btn-2");
+document.addEventListener("DOMContentLoaded", () => {
+  for (let i = 0; i < 16; i++) {
+    let column = document.createElement("div");
+    column.setAttribute("class", "div");
+    for (let j = 0; j < 16; j++) {
+      let row = document.createElement("div");
+      row.setAttribute("class", "div");
+      row.style.border = "2px solid black";
+      column.appendChild(row);
+      row.addEventListener("mouseenter", () => {
+        row.classList.toggle("hover");
+      });
+      btn2.addEventListener("click", () => {
+        row.classList.remove("hover");
+      });
+    }
+    container.appendChild(column);
+  }
+});
 
-function onClick() {
+btn.addEventListener("click", () => {
   container.innerHTML = "";
 
-  let num = +prompt("Enter the grid size: ","16");
+  let num = +prompt("Enter the grid size: ", "16");
   if (num > 100) {
     alert("Choose a smaller number");
   } else if (isNaN(num) || num === null || num === "" || num === " ") {
@@ -18,12 +38,14 @@ function onClick() {
         row.setAttribute("class", "div");
         row.style.border = "2px solid black";
         column.appendChild(row);
-        row.addEventListener('mouseenter',()=>{
-            row.classList.toggle('hover')
-        })
+        row.addEventListener("mouseenter", () => {
+          row.classList.toggle("hover");
+        });
+        btn2.addEventListener("click", () => {
+          row.classList.remove("hover");
+        });
       }
-      container.appendChild(column)
+      container.appendChild(column);
     }
   }
-}
-btn.addEventListener("click", onClick);
+});
